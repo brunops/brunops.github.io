117d23f96c6ca36b1e337296fbabda725cdb5d3f
Bruno Sanches <brunopsanches@gmail.com>
2014-01-12 06:44:29 -0800

    Add Bouncing balls project

---
+++ projects/bouncing-balls/bouncing-balls.js
@@ -0,0 +1,123 @@
+window.onload = (function() {
+
+  function Ball(x, y, radius, color) {
+    this.init(x, y, radius, color);
+  }
+
+  Ball.prototype.init = function(x, y, radius, color) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+
+    this.verticalSpeed = Math.floor(Math.random() * 3) + 2;
+    this.horizontalSpeed = Math.floor(Math.random() * 3) + 2;
+
+    // up === true
+    // down === false
+    this.vertical = Math.floor(Math.random() * 2) === 1;
+
+    // right ===  true
+    // left === false
+    this.horizontal = Math.floor(Math.random() * 2) === 1;
+  };
+
+  Ball.prototype.draw = function(context) {
+    context.fillStyle = this.color;
+    context.beginPath();
+    context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    context.closePath();
+    context.fill();
+  };
+
+  Ball.prototype.invertVertical = function() {
+    this.vertical = !this.vertical;
+  };
+
+  Ball.prototype.invertHorizontal = function() {
+    this.horizontal = !this.horizontal;
+  };
+
+  Ball.prototype.tick = function() {
+    this.x = this.x + (this.horizontal ? this.horizontalSpeed : - this.horizontalSpeed);
+    this.y = this.y + (this.vertical ? - this.verticalSpeed : this.verticalSpeed);
+  };
+
+  function BouncingBalls() {
+    this.init();
+  }
+
+  BouncingBalls.colors = [
+    'red',
+    'blue',
+    'indigo',
+    '#b4d455',
+    'slateblue',
+    '#333',
+    'orange'
+  ];
+
+  BouncingBalls.prototype.init = function() {
+    this.collection = [];
+
+    this.canvas  = document.getElementById('canvas');
+    this.context = canvas.getContext('2d');
+
+    this.bindEvents();
+
+    setInterval(this.tick.bind(this), 20);
+  };
+
+  BouncingBalls.prototype.bindEvents = function() {
+    var self = this;
+
+    this.canvas.addEventListener('click', function(e) {
+      var randomColor = BouncingBalls.colors[Math.floor(Math.random() * BouncingBalls.colors.length)],
+          rect = e.target.getBoundingClientRect(),
+          x = e.offsetX || e.pageX - rect.left - window.scrollX,
+          y = e.offsetY || e.pageY - rect.top - window.scrollY;
+
+      self.add(x, y, 10, randomColor);
+    });
+  };
+
+  BouncingBalls.prototype.tick = function() {
+    canvas.width = canvas.width;
+    for (var i = 0; i < this.collection.length; ++i) {
+      this.collection[i].tick();
+      this.handleNewCoords(this.collection[i]);
+      this.collection[i].draw(this.context);
+    }
+  };
+
+  BouncingBalls.prototype.handleNewCoords = function(ball) {
+    if (ball.x - ball.radius < 0) {
+      ball.x = ball.radius;
+      ball.invertHorizontal();
+    }
+
+    if (ball.x + ball.radius > this.canvas.width) {
+      ball.x = this.canvas.width - ball.radius;
+      ball.invertHorizontal();
+    }
+
+    if (ball.y - ball.radius < 0) {
+      ball.y = ball.radius;
+      ball.invertVertical();
+    }
+
+    if (ball.y + ball.radius > this.canvas.height) {
+      ball.y = this.canvas.height - ball.radius;
+      ball.invertVertical();
+    }
+  };
+
+  BouncingBalls.prototype.add = function(x, y, radius, color) {
+    var newBall = new Ball(x, y, radius, color);
+    newBall.draw(this.context);
+
+    this.collection.push(newBall);
+  };
+
+  new BouncingBalls();
+})();
---
+++ projects/bouncing-balls/index.markdown
@@ -0,0 +1,20 @@
+---
+layout: default
+title: Bouncing Balls
+---
+<style>
+  .canvas-wrapper {
+    border: 5px solid black;
+    width: 500px;
+    margin: 0 auto;
+  }
+</style>
+
+Click on the canvas
+<div class="canvas-wrapper">
+  <canvas id="canvas" width="500px" height="500px"></canvas>
+</div>
+
+Just some canvas play..
+
+<script src="bouncing-balls.js"></script>
